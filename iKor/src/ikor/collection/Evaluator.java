e042c18d167de9a2e02f7bbe250ecc7e51be56b8
fberzal@gmail.com <fberzal@gmail.com@4cf19b59-0b58-2337-37b0-77de530b535a>
2011-12-05 14:50:41 +0000

    [ikor] Evaluator interface

---
+++ iKor/src/ikor/collection/Evaluator.java
@@ -0,0 +1,19 @@
+package ikor.collection;
+
+// Title:       Evaluator
+// Version:     1.0
+// Copyright:   2008
+// Author:      Fernando Berzal
+// E-mail:      berzal@acm.org
+
+
+/**
+ * Evaluator
+ * 
+ * @author Fernando Berzal
+ */
+
+public interface Evaluator<T>
+{
+   public double evaluate (T object);
+}

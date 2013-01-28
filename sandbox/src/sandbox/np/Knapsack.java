371ec44846cb424b95a0d307aaa4a44ae93d5206
fberzal@gmail.com <fberzal@gmail.com@4cf19b59-0b58-2337-37b0-77de530b535a>
2013-01-28 11:44:18 +0000

    [sandbox] Knapsack problem: Approximation algorithm, O(n^3/epsilon)

--- sandbox/src/sandbox/np/Knapsack.java
+++ sandbox/src/sandbox/np/Knapsack.java
@@ -119,8 +119,94 @@ public class Knapsack
 		return knapsack;
 	}
 
+	// Approximation algorithm: O(n^2 vmax) == O(n^3 / epsilon) for a (1-epsilon)-approximation
+	
+	public int approximateSolution (float epsilon)
+	{
+		// Parameters
+		
+		int vmax;
+		int vsum = 0;
+		
+		vmax= Integer.MIN_VALUE;
+		
+		for (int i=0; i<n; i++) {
+			if (value[i]>vmax)
+				vmax = value[i];
+		}
+		
+		int m = (int) ((epsilon * vmax ) / n);
+		
+		int vapprox[] = new int[n];
+		
+		for (int i=0; i<n; i++) {
+			vapprox[i] = value[i] / m;
+			vsum += vapprox[i];
+		}
+		
+		System.out.println("m = "+m);
+		System.out.println("vsum = "+vsum);
+		
+		// Dynamic programming algorithm
+		
+		int previous[] = new int[vsum+1];
+		int current[] = new int[vsum+1];
+		int tmp[];
+		
+		previous[0] = 0;
+		
+		for (int x=1; x<=vsum; x++)
+			previous[x] = Integer.MAX_VALUE;
+		
+		for (int i=1; i<=n; i++) {
+			for (int x=0; x<=vsum; x++) {
+				
+				current[x] = previous[x];
+				
+				int alternative;
+				
+				if (vapprox[i-1]>=x)
+					alternative = weight[i-1];
+				else if (previous[x-vapprox[i-1]] == Integer.MAX_VALUE)
+					alternative = Integer.MAX_VALUE;
+				else
+					alternative = weight[i-1] + previous[x-vapprox[i-1]];
+				
+				if (alternative < current[x])
+					 current[x] = alternative;
+			}
+			
+			tmp = previous;
+			previous = current;
+			current = tmp;
+		}
+		
+		// Last row @ previous
+		
+		int xmax = 0;
+		
+		while ((previous[xmax]<=C) && (xmax<vsum))
+			xmax++;
+		
+		System.out.println(xmax);
+		
+		return m*(xmax-1);
+	}
 	
 	// Test program
+	
+	// e.g. 
+	//	500 objects
+	//	2000000 capacity
+	//	Optimal solution                     value = 2595819, time =  6915 ms
+	// 	Approximate solution (epsilon=0.01): value = 2595819, time = 39243 ms, error = 0.000%
+	// 	Approximate solution (epsilon=0.02): value = 2595780, time = 11467 ms, error < 0.002% 
+	// 	Approximate solution (epsilon=0.03): value = 2595755, time =  6903 ms, error < 0.003%
+	// 	Approximate solution (epsilon=0.05): value = 2595654, time =  3961 ms, error < 0.007% 
+	// 	Approximate solution (epsilon=0.10)  value = 2595514, time =  1900 ms, error < 0.012%
+	// 	Approximate solution (epsilon=0.20): value = 2595138, time =   942 ms, error < 0.027%
+	// 	Approximate solution (epsilon=0.30): value = 2594761, time =   656 ms, error < 0.041%
+	// 	Approximate solution (epsilon=0.50): value = 2593800, time =   374 ms, error < 0.078%
 	
 	public static void main (String args[])
 			throws IOException
@@ -142,7 +228,8 @@ public class Knapsack
 			chrono.start();
 			
 			//int solution = problem.iterativeDP();
-			int solution = problem.memoizedDP();
+			//int solution = problem.memoizedDP();
+			int solution = problem.approximateSolution(0.1f);
 			
 			chrono.stop();
 			

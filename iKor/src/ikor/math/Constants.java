b97227486b7a319c0f4084263f7e7643c7a5a908
fberzal@gmail.com <fberzal@gmail.com@4cf19b59-0b58-2337-37b0-77de530b535a>
2013-02-04 07:43:45 +0000

    [ikor.math] Special constants

--- iKor/src/ikor/math/Constants.java
+++ iKor/src/ikor/math/Constants.java
@@ -3,6 +3,7 @@ package ikor.math;
 public class Constants 
 {
 	public static final double SQRT2   = Math.sqrt(2);
+	public static final double SQRTPI  = Math.sqrt(Math.PI);
 	public static final double SQRT2PI = Math.sqrt(2*Math.PI);
 	public static final double LOG2PI  = Math.log(2*Math.PI);
 }

e3995c436954c10f1caaa3660f6e8ecd7a14fd7d
fberzal@gmail.com <fberzal@gmail.com@4cf19b59-0b58-2337-37b0-77de530b535a>
2013-02-04 13:41:46 +0000

    [sandbox] Statistical distributions: Wald distribution == Inverse Gaussian distribution, IG(mu,lambda)

---
+++ sandbox/src/sandbox/math/WaldDistribution.java
@@ -0,0 +1,86 @@
+package sandbox.math;
+
+import ikor.math.Configuration;
+import ikor.math.Constants;
+import ikor.math.Functions;
+
+/**
+ * Wald distribution (a.k.a.  inverse Gaussian distribution)
+ * http://en.wikipedia.org/wiki/Inverse_Gaussian_distribution
+ * 
+ * @author Fernando Berzal (berzal@acm.org)
+ */
+public class WaldDistribution implements Distribution 
+{
+	private double mu;
+	private double lambda;
+	
+	/**
+	 * Wald distribution == Inverse Gaussian distribution 
+	 * @param mu > 0 (mean)
+	 * @param lambda > 0 (shape parameter)
+	 */
+	public WaldDistribution (double mu, double lambda)
+	{
+		this.mu = mu;
+		this.lambda = lambda;
+	}
+	
+	@Override
+	public double pdf (double x) 
+	{
+		if (x<=0)
+			return 0;
+		else
+			return Math.sqrt(lambda/(x*x*x))/Constants.SQRT2PI * Math.exp ( -lambda*(x-mu)*(x-mu) / (2*mu*mu*x) );	
+	}
+
+	@Override
+	public double cdf (double x) 
+	{	
+		if (x<=0) {
+			return 0.0;
+		} else {
+			double d = x/mu;
+			double r = Math.sqrt(lambda/x);
+			return Functions.Phi(r*(d-1)) + Math.exp(2*lambda/mu)*Functions.Phi(-r*(d+1));
+		}
+	}
+
+	@Override
+	public double idf (double p) 
+	{
+		if (p==0)
+			return 0;
+		else if (p==1)
+			return Double.POSITIVE_INFINITY;
+		else
+			return idfsearch(p,0,1e6);
+	}
+	
+	// Bisection method:
+	// value k in [min,max] such that cdf(<k) <= p < cdf(<=k)
+
+	protected double idfsearch (double p, double min, double max)
+	{		
+		double v = min;
+		
+		while (max-min > Configuration.EPSILON) { 
+			v = (min+max)/2;
+			if (p <= cdf(v)) 
+				max = v;
+			else 
+				min = v;
+		}
+
+		return v;		
+	}
+	
+	@Override
+	public double random() 
+	{
+		// TODO Random number generator
+		return 0;
+	}
+
+}
---
+++ sandbox/src/sandbox/math/WaldDistributionTest.java
@@ -0,0 +1,177 @@
+package sandbox.math;
+
+import static org.junit.Assert.*;
+
+import ikor.math.Configuration;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WaldDistributionTest 
+{
+	public static double ERROR = Configuration.EPSILON;
+
+	@Before
+	public void setUp() throws Exception 
+	{
+	}
+
+	// IG(1,1)
+	
+	@Test
+	public void test11PDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(1,1);
+		
+		assertEquals( 0.0, distribution.pdf(0.0), ERROR );
+		assertEquals( 1/Math.sqrt(2*Math.PI), distribution.pdf( 1.0), ERROR );
+		assertEquals( 0.0, distribution.pdf(1e6), ERROR );
+	}
+	
+	@Test
+	public void test11CDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(1,1);
+		
+		assertEquals( 0.0, distribution.cdf(0.0), ERROR );
+		assertEquals( 1.0, distribution.cdf(1e6), ERROR );
+	}
+	
+	@Test
+	public void test11IDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(1,1);
+		
+		assertEquals( 0.0, distribution.idf(0.0), ERROR );
+		assertEquals( Double.POSITIVE_INFINITY, distribution.idf(1.0), ERROR );
+	}	
+	
+	// IG(1,2)
+
+	@Test
+	public void test12PDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(1,2);
+		
+		assertEquals( 0.0, distribution.pdf(0.0), ERROR );
+		assertEquals( 1/Math.sqrt(Math.PI), distribution.pdf( 1.0), ERROR );
+		assertEquals( 0.0, distribution.pdf(1e6), ERROR );
+	}
+	
+	@Test
+	public void test12CDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(1,2);
+		
+		assertEquals( 0.0, distribution.cdf(0.0), ERROR );
+		assertEquals( 1.0, distribution.cdf(1e6), ERROR );
+	}
+	
+	@Test
+	public void test12IDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(1,2);
+		
+		assertEquals( 0.0, distribution.idf(0.0), ERROR );
+		assertEquals( Double.POSITIVE_INFINITY, distribution.idf(1.0), ERROR );
+	}
+
+	// IG(2,1)
+
+	@Test
+	public void test21PDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(2,1);
+		
+		assertEquals( 0.0, distribution.pdf(0.0), ERROR );
+		assertEquals( 1/Math.sqrt(2*Math.PI*8), distribution.pdf(2.0), ERROR );
+		assertEquals( 0.0, distribution.pdf(1e6), ERROR );
+	}
+	
+	@Test
+	public void test21CDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(2,1);
+		
+		assertEquals( 0.0, distribution.cdf(0.0), ERROR );
+		assertEquals( 1.0, distribution.cdf(1e6), ERROR );
+	}
+	
+	@Test
+	public void test21IDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(2,1);
+		
+		assertEquals( 0.0, distribution.idf(0.0), ERROR );
+		assertEquals( Double.POSITIVE_INFINITY, distribution.idf(1.0), ERROR );
+	}
+	
+	// IG(5,2)
+	
+	@Test
+	public void test52PDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(5,2);
+		
+		assertEquals( 0.0, distribution.pdf(0), ERROR );
+		assertEquals( 1/Math.sqrt(Math.PI*5*5*5), distribution.pdf(5.0), ERROR );
+		assertEquals( 0.0, distribution.pdf(1e6), ERROR );
+	}
+	
+	
+	@Test
+	public void test52CDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(5,2);
+		
+		assertEquals( 0.0, distribution.cdf(0.0), ERROR );
+		assertEquals( 1.0, distribution.cdf(1e6), ERROR );
+	}
+	
+	@Test
+	public void test52IDF() 
+	{
+		WaldDistribution distribution = new WaldDistribution(5,2);
+		
+		assertEquals( 0.0, distribution.idf(0.0), ERROR );
+		assertEquals( Double.POSITIVE_INFINITY, distribution.idf(1.0), ERROR );
+	}
+	
+	// Intervals
+	
+	@Test
+	public void testIntervals ()
+	{
+		WaldDistribution distribution = new WaldDistribution(1,1);
+		
+		assertEquals( 0.668102001223, distribution.cdf(1), ERROR);
+		assertEquals( 0.885475429860, distribution.cdf(2), ERROR);
+		assertEquals( 0.953187920743, distribution.cdf(3), ERROR); // Three sigma
+		assertEquals( 0.979076364179, distribution.cdf(4), ERROR);
+		assertEquals( 0.990115297400, distribution.cdf(5), ERROR);
+		assertEquals( 0.995150117866, distribution.cdf(6), ERROR); // Six sigma
+	}
+	
+	// Quantile function: z_p
+	
+	@Test
+	public void testQuantiles ()
+	{
+		WaldDistribution distribution = new WaldDistribution(1,1);
+
+		assertEquals( 0.675841249631, distribution.idf(0.50), ERROR);
+		assertEquals( 1.244059774308, distribution.idf(0.75), ERROR);
+		assertEquals( 2.143033896118, distribution.idf(0.90), ERROR);
+		assertEquals( 2.922076021150, distribution.idf(0.95), ERROR);
+		assertEquals( 4.984094800875, distribution.idf(0.99), ERROR);
+		assertEquals( 8.354864974081, distribution.idf(0.999), ERROR);
+
+		assertEquals( 0.50, distribution.cdf(0.675841249631), ERROR);
+		assertEquals( 0.75, distribution.cdf(1.244059774308), ERROR);
+		assertEquals( 0.90, distribution.cdf(2.143033896118), ERROR);
+		assertEquals( 0.95, distribution.cdf(2.922076021150), ERROR);
+		assertEquals( 0.99, distribution.cdf(4.984094800875), ERROR);
+		assertEquals( 0.999,distribution.cdf(8.354864974081), ERROR);
+	}
+}
+

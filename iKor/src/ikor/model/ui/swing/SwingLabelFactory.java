5f71bf318f5266fac404780b49ce37fa6c93480d
fberzal@gmail.com <fberzal@gmail.com@4cf19b59-0b58-2337-37b0-77de530b535a>
2014-12-05 09:32:08 +0000

    [ikor.ui] Swing application generator - JTextPane instead of JLabel to support clickable links...

--- iKor/src/ikor/model/ui/swing/SwingLabelFactory.java
+++ iKor/src/ikor/model/ui/swing/SwingLabelFactory.java
@@ -1,9 +1,14 @@
 package ikor.model.ui.swing;
 
+import java.awt.Desktop;
+
 import ikor.model.ui.Label;
 import ikor.model.ui.UIFactory;
 
-import javax.swing.JLabel;
+import javax.swing.JEditorPane;
+import javax.swing.JTextPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 
 public class SwingLabelFactory implements UIFactory<SwingUI,Label>
@@ -11,16 +16,42 @@ public class SwingLabelFactory implements UIFactory<SwingUI,Label>
 	@Override
 	public void build (SwingUI ui, Label label) 
 	{
-		JLabel jlabel = new JLabel();
+		JTextPane jlabel = new JTextPane();
 		
-		jlabel.setText( label.getText() );
-				
+		jlabel.addHyperlinkListener(new LabelHyperlinkListener());
+	    jlabel.setEditable(false);
+	    jlabel.setOpaque(false);
+	    jlabel.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true);
+	    
+	    jlabel.setContentType("text/html");
+	    jlabel.setText(label.getText());
+		
 		if (label.getDescription()!=null)
 			jlabel.setToolTipText( label.getDescription() );
 
-		if (label.getIcon()!=null)
-			jlabel.setIcon( ui.loadIcon(label.getIcon()) );
+		//if (label.getIcon()!=null)
+		//	jlabel.setIcon( ui.loadIcon(label.getIcon()) );
 		
 		ui.addComponent ( jlabel );	
+	}
+	
+	// Hyperlink listeners
+	
+	class LabelHyperlinkListener implements HyperlinkListener
+	{
+		@Override
+		public void hyperlinkUpdate(HyperlinkEvent event) 
+		{
+		    if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+		    	
+		    	if (Desktop.isDesktopSupported()) {
+		    	      try {
+		    	        Desktop.getDesktop().browse(event.getURL().toURI());
+		    	      } catch (Exception error) {
+		    	      }
+		    	}
+		    }			
+		}
+		
 	}
 }
